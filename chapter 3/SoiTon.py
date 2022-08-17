a298b54c49fb2af90a24fab6d0ed3c8a9f301069
Rattanaporn <88606135+RSMMIX@users.noreply.github.com>
2022-08-17 14:20:19 +0700

    LabPython

--- chapter 3/SoiTon.py
+++ chapter 3/SoiTon.py
@@ -1,1 +1,47 @@
-n = input('Enter Input : ')
+'''ที่จอดรถของนาย ก เป็นส่วนที่แรเงาสีฟ้า ส่วนสีแดงเป็นที่ของนาย ข ซี่งเป็นญาติกัน ที่จอดรถของนาย ก และ นาย ข แคบมาก จอดรถได้เรียงเดี่ยว นาย ข ไม่ได้ใช้ที่จอดรถ แต่ อนุญาติให้นาย ก ใช้ที่จอดรถของเขาได้โดยไม่จอดรถแช่ไว้ เนื่องจากซอยแคบ ดังนั้นการมาจอด (arrive) และการรับรถ (depart)จะเป็นลักษณะของ stack เงื่อนไขคือ ในการรับรถ x ใดๆอยากให้ลำดับรถเป็นเหมือนเดิม ดังรูป simulate การจอดรถในที่จอดรถของนาย ก โดยใช้ operation ของ stack ข้างล่างเป็นตัวอย่าง output
+
+การรับ input : รับ input 4 ค่าใน 1 บรรทัดโดยให้แยกโดย " " (space bar) โดยตำแหน่งแรกคือ จำนวนสูงสุดที่รถสามารถจอดได้ในซอยของ นาย ก ตำแหน่งที่สองคือ รถที่จอดอยู่ในซอยของ นาย ก ตำแหน่งที่สามคือ การกระทำเช่น ถ้าเป็น arrive จะทำการเพิ่มรถในซอย ส่วน depart จะทำการเอารถออกจากซอย โดยรถที่จะทำการเพิ่มหรือนำออกนั้นจะเป็น เลขในตำแหน่งที่ 4
+
+***หมายเหตุ ถ้าในซอยไม่มีรถอยู่เลยให้ input = 0 ในตำแหน่งที่ 2***
+
+*** สามารถสร้างได้มากกว่า 1 stack ***
+
+'''
+
+class Stack:
+    def __init__(self,lst = None):
+        if lst == None:
+            self.items = []
+        else:
+            self.items = lst
+
+    def push(self,i):
+        self.items.append(i)
+           
+    def pop(self):
+        return self.items.pop(len(self.items)-1)
+
+    def isEmpty(self):
+        return self.items == []
+        
+    def size(self):
+        return (len(self.items))
+
+print('******** Parking Lot ********')
+
+m,s,o,n = input("Enter max of car,car in soi,operation : ").split()
+stack = list(s.split(','))
+m,n = int(m),int(n)
+
+if o == 'arrive':
+    if len(stack) < m :
+        if n in stack:
+            print(f"car {n} already in soi")
+
+        else:
+            print(f'car {n} arrive! : Add Car {n}')
+            stack.append(n)
+    
+    else:
+        print(f'car {n} cannot arrive : Soi Full ')
+
