d432c5f3f31873949653089c2ac556fd36eea15b
Rattanaporn <88606135+RSMMIX@users.noreply.github.com>
2022-10-19 00:47:29 +0700

    tree

---
+++ Tree 2 (AVL)/Ranking.py
@@ -0,0 +1,66 @@
+"""
+จงเขียนฟังก์ชั่นในการหา Rank ของ input ที่รับเข้ามา โดย Rank คือการแบ่งเป็นชั้นๆตามข้อมูลของ BST โดยจะเริ่มจากค่าที่น้อยกว่าค่าใน BST ที่น้อยที่สุดจะมีค่า Rank = 0 และค่าที่อยู่ตั้งแต่ค่าที่น้อยที่สุดจนถึงตัวถัดไปจะมีค่า Rank +=1 ไปเรื่อยๆจนถึงชั้นของตัวสุดท้ายหรือตัวมากสุด เช่น
+
+
+
+จากรูป ค่าที่น้อยที่สุดคือ -2 ดังนั้น rank(-2) จะได้ 1 แต่ rank ของค่าที่น้อยกว่า -2 จะเท่ากับ 0
+
+และ rank(0) จะเท่ากับ 1 ส่วน rank(1) จะเท่ากับ 2 เป็นต้น
+"""
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+    
+    def __str__(self):
+        return str(self.data)
+
+class BST:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, data):
+        if self.root == None:
+            self.root = Node(data)
+        else:
+            cur = self.root
+            while True:
+                if data < cur.data:
+                    if cur.left == None:
+                        cur.left = Node(data)
+                        break
+                    else:
+                        cur = cur.left
+                else:
+                    if cur.right == None:
+                        cur.right = Node(data)
+                        break
+                    else:
+                        cur = cur.right
+        return self.root
+
+    def Rank(self,node,data):
+        if node == None:
+            return 0
+        rank = 0
+        rank += self.Rank(node.left,data)
+        if node.data <= data:
+            rank += 1
+        rank += self.Rank(node.right,data)
+        return rank
+
+    def printTree(self, node, level = 0):
+        if node != None:
+            self.printTree(node.right, level + 1)
+            print('     ' * level, node)
+            self.printTree(node.left, level + 1)
+
+bst = BST()
+lists,k = input("Enter Input : ").split("/")
+for i in lists.split():
+    root = bst.insert(int(i))
+bst.printTree(root)
+print("--------------------------------------------------")
+print("Rank of "+k+" : "+str(bst.Rank(root,int(k))))
