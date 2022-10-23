5be0e808d770bfd895b5a1f9e058a0aeb102553b
Rattanaporn <88606135+RSMMIX@users.noreply.github.com>
2022-10-23 18:50:01 +0700

    Lab8

--- Tree 2 (AVL)/van.py
+++ Tree 2 (AVL)/van.py
@@ -11,7 +11,7 @@
 
 3
 """
-num_van, cus = input('Enter Input : ').split('/')
+"""num_van, cus = input('Enter Input : ').split('/')
 num_van = int(num_van)
 cus = list(map(int, cus.split()))
 van = 1
@@ -19,4 +19,33 @@ for i in range(len(cus)):
     if van > num_van:
         van = 1
     print(f'Customer {i+1} Booking Van {van} | {cus[i]} day(s)')
-    van += 1
+    van += 1"""
+
+
+def printTree(data, index, size, level):
+    if  index < size:
+        printTree(data, 2 * index + 2, size, level + 1)
+        print('        ' * level, data[index])
+        printTree(data, 2 * index + 1, size, level + 1)
+
+
+inp = input('Enter Input : ').split('/')
+
+data = []
+van = {}
+
+for name in range(int(inp[0])):
+    van[name + 1] = van.get(name + 1, 0)
+    data.append(int(name) + 1)
+
+for cus, key in enumerate(inp[1].split()):
+    print(f'Customer {cus + 1} Booking Van {data[0]} | {key} day(s)')
+    temp = data.pop(0)
+    van[temp] = van.get(temp, 0) + int(key)
+    for index, key in enumerate(data):
+        if  van[temp] < van[key] or (van[temp] == van[key] and temp < key):
+            data.insert(index, temp)
+            temp = None
+            break
+    if temp:
+        data.append(temp)
