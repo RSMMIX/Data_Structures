4e0737f7b8fe1055adb0e841046e471848454d42
Rattanaporn <88606135+RSMMIX@users.noreply.github.com>
2022-11-22 01:26:16 +0700

    Lab

--- Sorting/SortSubset.py
+++ Sorting/SortSubset.py
@@ -32,3 +32,33 @@
 [-3, -1, 0, 1, 2, 3]
 
 """
+def printRecur(num, left, res, x=0, list=[]):
+    sum = 0
+    patten = 0
+    for i in list:
+        sum += i
+    if left == 0:
+        if res == sum:
+            print(list)
+            return 1
+        return 0
+    elif x >= len(num):
+        return 0
+    else:
+        patten += printRecur(num, left-1, res, x + 1, list + [num[x]])
+        patten += printRecur(num, left, res, x + 1, list)
+    return patten
+
+
+inp = input('Enter Input : ').split("/")
+res = int(inp[0])
+num = [int(x) for x in inp[1].split()]
+for i in range(len(num)):
+    for j in range(len(num)-1-i):
+        if num[j] > num[j+1]:
+            num[j], num[j+1] = num[j+1], num[j]
+patten = 0
+for j in range(len(num)):
+    patten += printRecur(num, j+1, res)
+if patten == 0:
+    print('No Subset')
--- Sorting/Straight SelectionSort[recursive].py
+++ Sorting/Straight SelectionSort[recursive].py
@@ -7,3 +7,21 @@
 
 *** ยกเว้นให้ใช้  for ได้แค่ขั้นตอนรับ input เท่านั้น ***
 """
+
+def str_selection(n, k, m, i):
+    if n == len(inp) - 1:
+        return
+    if k == len(inp) - n:
+        inp[i], inp[len(inp) - n - 1] = inp[len(inp) - n - 1], inp[i]
+        if len(inp) - n - 1 != i:
+            print(f'swap {inp[i]} <-> {inp[len(inp) - n - 1]} : {inp}')
+        str_selection(n+1, 0, -1, -1)
+        return
+    if inp[k] > m:
+        str_selection(n, k+1, inp[k], k)
+    else:
+        str_selection(n, k+1, m, i)
+
+inp = list(map(int, input('Enter Input : ').split()))
+str_selection(0, 0, -1, 0)
+print(inp)
